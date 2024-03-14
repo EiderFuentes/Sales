401ec7afc847c324bc9f01b2c2957106ac66bdf6
Eider Fuentes <ing.eiderfuentes1989@gmail.com>
2024-03-14 18:38:20 -0500

    Seed Db V1 finish

---
+++ Sales/Sales.API/Data/SeedDb.cs
@@ -0,0 +1,38 @@
+﻿using Sales.Shared.Entites;
+
+namespace Sales.API.Data
+{
+    //
+    public class SeedDb
+    {
+        private readonly DataContext _context;
+
+        public SeedDb(DataContext context)
+        {
+            _context = context;
+        }
+
+        //Metodo para alimentar la base de datos
+        public async Task SeedAsync()
+        {
+            //Metodo para llenar la base de datos
+            await _context.Database.EnsureCreatedAsync();
+            await CheckCountriesAsync();
+        }
+
+        private async Task CheckCountriesAsync()
+        {
+            //Valida si no hay registro en la base de dato
+            if (!_context.Countries.Any())
+            {
+                _context.Countries.Add(new Country { Name = "Colombia" });
+                _context.Countries.Add(new Country { Name = "Estados Unidos" });
+                _context.Countries.Add(new Country { Name = "Perú" });
+                _context.Countries.Add(new Country { Name = "México" });
+                await _context.SaveChangesAsync();
+            }
+     
+        }
+    }
+}
+
--- Sales/Sales.API/Program.cs
+++ Sales/Sales.API/Program.cs
@@ -9,8 +9,23 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 //Inyectamos nuestra cadena de conexion
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
+//Inyectamos SeedBd por la forma  AddTransient una sola vez
+builder.Services.AddTransient<SeedDb>();
 
 var app = builder.Build();
+// Inyecion como un constructor
+SeedData(app);
+
+void SeedData(WebApplication app)
+{
+    IServiceScopeFactory? scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using (IServiceScope? scope = scopedFactory!.CreateScope())
+    {
+        SeedDb? service = scope.ServiceProvider.GetService<SeedDb>();
+        service!.SeedAsync().Wait();
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
