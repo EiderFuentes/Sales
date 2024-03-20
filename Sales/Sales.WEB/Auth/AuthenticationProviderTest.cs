303201666ffd0f725edfcbc8ffb23e9b6410693a
Eider Fuentes <ing.eiderfuentes1989@gmail.com>
2024-03-19 21:51:06 -0500

    Security hardcoded on Web

---
+++ Sales/Sales.WEB/Auth/AuthenticationProviderTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Sales.WEB.Auth
+{
+    // Clase de autenticacion
+    public class AuthenticationProviderTest : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            //await Task.Delay(3000);
+            //Autenticacion generica hardcodeada
+            var anonimous = new ClaimsIdentity();//Creo un usuario que no es anonimo
+            var zuluUser = new ClaimsIdentity(new List<Claim>
+             {
+                //lista reclamaciones
+                new Claim("FirstName", "Juan"),//Usuario Administrador
+                new Claim("LastName", "Zulu"),
+                new Claim(ClaimTypes.Name, "zulu@yopmail.com"),
+                new Claim(ClaimTypes.Role, "Admin")
+
+             },
+             authenticationType: "test");
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonimous)));
+        }
+
+    }
+}
